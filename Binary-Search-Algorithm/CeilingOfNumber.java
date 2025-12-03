3fc27bccd1674ef944edfc02f60e4ade6d47cc93
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2025-12-03 19:01:03 +0530

    Q. Finding Ceiling of a Number

---
+++ Binary-Search-Algorithm/CeilingOfNumber.java
@@ -0,0 +1,29 @@
+// Q. Finding Ceiling of a Number
+
+
+// Ceiling of a number is a smallest number >= target
+public class CeilingOfNumber {
+    public static int ceilingNumber(int arr[], int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(arr[mid] == target){
+                return arr[mid];
+            }else if(arr[mid] > target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+
+        return arr[start];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 5, 9, 14, 16, 18};
+        System.out.println(ceilingNumber(arr, 10));
+    }
+}
