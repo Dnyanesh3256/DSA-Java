a98f31c157410a174cf90543bae9431cf4912b93
Dnyaneshwar Tupe <dbtupe3256@gmail.com>
2026-03-23 09:44:52 +0530

    Q. Print linearly from N to 1.

---
+++ Basics/Basic-Recursion/PrintLinearlyDec.java
@@ -0,0 +1,17 @@
+// Q. Print linearly from N to 1.
+
+public class PrintLinearlyDec {
+    public static void print(int n){
+        if(n == 0){
+            return;
+        }
+
+        System.out.println(n);
+
+        print(n-1);
+    }
+
+    public static void main(String[] args) {
+        print(10);
+    }
+}
